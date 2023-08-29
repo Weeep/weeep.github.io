57287636cfc162b5f3e653b67a731f4f18b94e88
Dániel Nováki <szept26@gmail.com>
2023-08-29 22:35:05 +0200

    update

---
+++ scripts.js
@@ -0,0 +1,167 @@
+function changeContent(contentId) {
+    //document.getElementById('submenu-content').style.display = 'none'
+    //document.getElementById("submenu").querySelector(".submenu-content").classList.add("displayNone");
+
+    document.getElementById("page").innerHTML = `
+    <h3>Mi az az idegrendszeri éretlenség?</h3>
+    
+    <p>Az idegrendszerünk komplex, a teljes emberi működést sokféleképpen meghatározó tényező,
+    összetettségét nehéz is igazán átlátni laikusként. A fejlődését azonban nyomon tudjuk
+    követni azoknak a területeknek a fejlődésén keresztül, melyeket szabályoz. Így a mozgás-,
+    érzékelés rendszereinek működése, illetve ezek összehangoltságának mértéke segíthet az
+    eligazodásban. A rendszer komplexitásából következik, hogy a részek egymással
+    együttműködnek, egymásra hatással vannak, így az egyes területek erőssége, gyengesége
+    hatással van a többi működésére is. Így az éretlenség jelei sokfélék lehetnek, sokszor együtt
+    járnak önszabályozási-, érzelmi-, illetve kapcsolati nehézséggel.</p>
+    <p>Ha észrevesszük az éretlenség jeleit, gyakran a kialakulás okát szeretnénk tudni, biztonságot
+    nyújtó lenne az egyértelműség ebben a kérdésben. Azonban ezt csak ritka esetben tudjuk
+    biztosan meghatározni. Ismerünk hajlamosító tényezőket, amik egészen magzati kortól
+    kezdve befolyással bírnak az idegrendszer fejlődésére. Amit viszont ezzel szemben biztosan
+    állíthatunk, hogy az érés kódolva van a szervezetünkben, és különféle módszerekkel
+    támogatható is. Egy ezek közül a dinamikus szenzoros integrációs terápia.</p>
+    
+    <h3>Mit jelent a dinamikus szenzoros integrációs terápiás szemlélet?</h3>
+    
+    <p>A szenzoros integráció folyamatában a különféle érzékelési csatornákon bejövő, környezetből
+    származó információ megfelelő feldolgozása, és az arra való adaptív válasz kialakítása
+    (mozgásos, beszéd, stb.) történik.</p>
+    <p>A foglalkozásaink célja, hogy az idegrendszer érését az
+    érzékelő és mozgásos rendszerek működtetésének ösztönzése, összehangolódásuk növelése
+    által támogassuk, (konkrétan a vesztibuláris-, taktilis-, és proprioceptív rendszer ingerlése
+    által). Mindez egy játékos keretben történik, mivel a szemléletünk szerint a gyermek
+    biztonságos, azonban érdeklődését felkeltő fizikai-, illetve elfogadó, saját indíttatásaira
+    válaszkész kapcsolati térben képes megtalálni a fejlődését szolgáló egyéni és örömteli utakat.</p>
+    <p>Fantáziája, érzelmi- és kapcsolati működése a motoros rendszerrel egyszerre aktiválódhat. A
+    köztünk lévő térbe, a játék varázslatos világán keresztül bekerülhetnek a gyermek vívódásai,
+    konfliktusai. Ezekkel azonban nem marad egyedül, eszköztelenül. Szakemberként a mi
+    feladatunk, hogy a gyermek folyamatait kísérjük, érzéseit elfogadjuk, tükrözzük, és olyan
+    teret biztosítsunk, amiben saját maga képessé válik megküzdeni nehézségeivel (legyenek
+    azok fizikai, vagy lelki természetűek), saját erősségeire támaszkodva az egyéni tempójában
+    megtanuljon hatékonyan válaszolni a környezete kihívásaira. Így a tevékenység komplex
+    módon hat, az idegrendszer érlelése mellett hozzájárul az egész személyiség harmonikusabb
+    működéséhez.</p>
+
+    <h3>Ki lehet érintett?</h3>
+    <ul>
+    <li>A gyermekek, akik nyugtalan, nehezen altatható csecsemők voltak, esetleg manapság is
+    nehézséggel küzdenek az alvás terén.</li>
+    <li>Akik &quot;hiperaktívnak&quot; tűnnek, nem tudnak nyugodtan maradni akár egy családi étkezés
+    erejéig sem. Akik sosem fáradnak el, mindig mozgásban vannak, a határokat feszegetik,
+    túlzottan vakmerőnek tűnnek.</li>
+    <li>Vagy éppen az előzőek ellentéte, kifejezetten fáradékonyak, kevéssé terhelhetők, időnként
+    &quot;lustának&quot; tűnnek. Bátortalanok, az új élményektől inkább visszahúzódnak.</li>
+    <li>Akik nem bújósak, nem szeretnek kézenfogva sorban állni, nem viselik jól bizonyos
+    ruhadarabok érintését, viselését.</li>
+    <li>Vagy épp az ellenkezője, olyanok, mint &quot;elefánt a porcelánboltban&quot;, mindenkivel
+    kapcsolatot teremtenek, a másik ember intim terét nem igazán érzékelik, a társaikkal
+    túlságosan vehemensen igyekeznek kapcsolódni, ezzel akár fájdalmat okozva a másiknak.</li>
+    <li>Akik érzékenyek a hangokra, hirtelen zajra befogják a fülüket, vagy épp kifejezetten keresik
+    és akár maguk keltik a zajokat.</li>
+    <li>Akikkel a nap már rosszul kezdődik, a készülődés egy tortúra, nehezen váltanak
+    tevékenységet. Egy-egy feladat végrehajtása közben állandóan elterelődnek, nem tudják
+    tartani a figyelmüket.</li>
+    <li>Akikért az óvodába aggodalommal telve érkeznek a szülők. Akik szülei értetlenül hallgatják
+    a gyermekük pedagógusát, mivel a hallottakat nem tudják összeegyeztetni az otthoni
+    tapasztalatokkal.</li>
+    <li>Akik közösségben feszültté válnak, nem tudják jól kezelni a konfliktusokat. Túlzottan
+    felpörögnek vagy épp nehezen barátkoznak, félénkek.</li>
+    <li>Akiknél a mozgásfejlődés késik/ kimaradt valamelyik szakasz/ esetleg a normál ütemnél
+    gyorsabban követték egymást az állomások</li>
+    <li>Akik a beszédfejlődésben lemaradással küzdenek.</li>
+    <li>Akik nem szeretnek rajzolni.</li>
+    <li>Akik ügyetlennek tűnnek, sokszor nekimennek bútoroknak, esnek-kelnek, levernek,
+    kiöntenek, összetörnek dolgokat.</li>
+    <li>Akik nehezen koncentrálnak, nem tartják a szemkontaktust, gyorsan váltanak a játékok
+    között, esetleg gyakran panaszkodnak arra, hogy &quot;unatkoznak&quot;.</li>
+    <li>Akiknek nehézségeik vannak a térbeli-, és időbeli tájékozódással.</li>
+    <li>Az “egyemberes” gyermekek</li>
+    </ul>
+    Csoportos vagy egyéni ellátás?
+     Miért igyekszünk zárt csoportokkal dolgozni?
+    Alapvetően állandó csoportokkal dolgozunk, mert a gyermekek biztonságérzetéhez, illetve a
+    kapcsolódásaik minőségi változásához a kiszámíthatóság szükséges. A közös térben az
+    együttes élménynek nagy szerepe van. A csoportban a gyermekek közös szabályokat,
+    játékokat alkotnak, saját szokásaik lesznek, ami az együttes sikerük, de az egyéneket külön-
+    külön is új, a korábbiaknál magasabb szintű készségekkel ruházza fel. Egymás fejlődésének
+    mozgatórugói, visszajelzői, tanúi. Időnként egymás riválisai, ellenlábasai, de ezekből a
+    konfliktusokból is együtt építkezünk. Felismerjük, megértjük, elfogadjuk, megtanuljuk
+    
+    kommunikálni az érzéseket, feszültségeket konstruktív módon. Ehhez a lassú, közös
+    növekedéshez, éréshez azonban elengedhetetlen az állandó jelenlét. 
+    Mitől függ, hogy egyéni vagy csoportos ellátást javaslunk?
+    A tapasztalataink alapján az idegrendszeri éretlenség következtében kialakult viselkedési
+    nehézségek esetén a csoportos ellátást javasoljuk általánosságban. A fenti bekezdésben
+    olvashatók a csoportos élmény előnyei. Hiszünk abban, hogy a csoporthelyzetben a
+    gyermekek intenzívebben hozzák a nehézségeiket, így gyorsabban ismerhetjük és érthetjük 
+    meg egy-egy gyermek működését. Azonban előfordul olyan eset is, hogy egy gyermek a
+    korához képest nagyobb lemaradással küzd, vagy olyan speciális élethelyzetben van, ami
+    miatt kiemelt figyelmet igényel, így számára a csoport nem lenne elegendő, ez indokolttá
+    teheti a gyermek egyéni ellátását. Az ellátás formájára való javaslattételünket minden esetben
+    a szülővel történő egyeztetés és a gyermekkel való találkozás előzi meg. 
+    Ez csak játék?!
+    A sokszínű tevékenységet felkínáló tér és eszközök kíváncsiságot, érdeklődést felkeltő,
+    mozgásra hívó, örömöt, lelkesedést kiváltó folyamatot indítanak a gyerekekben, tehát valóban
+    játszanak.
+    Amitől mégis több lesz, mint egy játék, az egyrészt a pszichológus jelenlétnek, másrészt a
+    speciális ingereket biztosító terápiás térnek köszönhető.
+    A pszichológusok jelenlétének szerepe ebben a folyamatban egyfelől a csoporttagok egyéni
+    szintjének, kezelésének, és fejlődésének megítélése, másfelől katalizátorként serkentik,
+    segítik azt a fejlődést, amelyet a gyerekek saját aktív részvételükkel, önszántukból, egyéni
+    útjukon járva érnek el.
+    A meleg, elfogadó, biztonságos közeg és azok a játékszerek, amelyek a vesztibuláris, taktilis
+    rendszerük, illetve saját testük érzékelése számára biztosítják az ingereket, együttesen segítik
+    az agyféltekék közötti kapcsolat megerősödését, az idegrendszer harmonizációját, támogatják
+    a tanulási, megismerési folyamatokat.`
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const submenus = document.querySelectorAll(".submenu");
+
+    submenus.forEach(function (submenu) {
+        submenu.addEventListener("click", function () {
+            const submenuContent = this.querySelector(".submenu-content");
+            submenuContent.style.display = "none";
+        });
+
+        submenu.addEventListener("mouseover", function () {
+            const submenuContent = this.querySelector(".submenu-content");
+            submenuContent.style.display = "block";
+        });
+    });
+});
+
+
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    const menuLinks = document.querySelectorAll(".menu-link");
+    const mainContent = document.getElementById("mainContent");
+
+    // Load content using AJAX
+    function loadContent(url) {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                mainContent.innerHTML = xhr.responseText;
+            }
+        };
+        xhr.send();
+    }
+
+    // Attach event listeners to menu links
+    menuLinks.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Prevent default link behavior
+            const pageUrl = link.getAttribute("href");
+            history.pushState(null, null, pageUrl); // Update the URL
+            loadContent(pageUrl); // Load content using AJAX
+        });
+    });
+
+    // Handle back/forward navigation
+    window.addEventListener("popstate", function () {
+        const pageUrl = location.href;
+        loadContent(pageUrl);
+    });
+});
+*/
